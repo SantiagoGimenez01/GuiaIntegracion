9a745f48ad0622b52ce390a107e0566cac417373
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-08-09 16:40:15 -0300

    Ejercicios 1 y 2 de Subtes

--- subtes.pl
+++ subtes.pl
@@ -17,10 +17,29 @@ combinacion([corrientes, pueyrredonB]).
 
 % Punto 1
 % En qué linea está una estación, predicado estaEn/2.
+estaEn(Estacion, Linea):-
+    linea(Linea, Estaciones),
+    member(Estacion, Estaciones).
 
 % Punto 2
 % Dadas dos estaciones de la misma línea, cuántas estaciones hay entre ellas, p.ej. entre Perú y Primera Junta hay 5 estaciones. 
 % Predicado distancia/3 (relaciona las dos estaciones y la distancia).
+distancia(EstacionOrigen, EstacionDestino, Distancia):-
+    linea(Linea, Estaciones),
+    estaEn(EstacionOrigen, Linea),
+    estaEn(EstacionDestino, Linea),
+    nth1(IndiceOrigen, Estaciones, EstacionOrigen),
+    nth1(IndiceDestino, Estaciones, EstacionDestino),
+    Distancia is abs(IndiceDestino - IndiceOrigen).
+
+
+% Punto 3
+% Dadas dos estaciones de distintas líneas, si están a la misma altura (o sea, las dos terceras, las dos quintas, etc.), p.ej. Independencia C y Jujuy 
+% que están las dos cuartas. Predicado mismaAltura/2.
+
+% Punto 4
+% Dadas dos estaciones, si puedo llegar fácil de una a la otra, esto es, si están en la misma línea, o bien puedo llegar con una sola combinación. 
+% Predicado viajeFacil/2.
 
 
 
