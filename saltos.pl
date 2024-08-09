92a60edad56c92d551154cdf2d8e8c0d7497089a
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-08-09 15:36:16 -0300

    Emprolijacion de ejercicio

--- saltos.pl
+++ saltos.pl
@@ -24,11 +24,6 @@ estaDescalificado(Competidor):-
 % Si un competidor clasifica a la final. Un competidor clasifica a la final si la suma de sus puntajes es mayor o igual a 28, o si tiene dos saltos de 8 
 % o más puntos.
 
-puntajes(hernan,[3,5,8,6,9]).
-puntajes(julio,[9,7,3,9,10,2]).
-puntajes(ruben,[3,5,3,8,3]).
-puntajes(roque,[7,10,10]).
-
 clasificaAFinal(Competidor):-
     sumaDePuntajesAlta(Competidor),
     not(estaDescalificado(Competidor)).
